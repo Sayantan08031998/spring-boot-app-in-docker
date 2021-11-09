487f0dadcaac31602aa3ec2fed2f080ac210e6f9
Debatri2021 <92325478+Debatri2021@users.noreply.github.com>
2021-11-09 20:32:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
 # Build image
 docker build -t $aws_ecr_repository_url_with_tag .
 
-aws ecr get-login-password --no-include-email
+aws ecr get-login-password
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
