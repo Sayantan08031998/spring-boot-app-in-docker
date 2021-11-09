cd83faec27bed2dd5b84850a994fd82d062da873
kshkraja <50064073+kshkraja@users.noreply.github.com>
2021-11-09 17:07:13 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fail fast
+set -e
+
+# This is the order of arguments
+aws_ecr_repository_url_with_tag=$1
+
+# Some Useful Debug
+echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
+
+# Build image
+docker build -t $aws_ecr_repository_url_with_tag .
+
+# Push image
+docker push $aws_ecr_repository_url_with_tag
