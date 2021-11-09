77f25659203924709c5be15e478472b256f481c4
Debatri2021 <92325478+Debatri2021@users.noreply.github.com>
2021-11-09 19:18:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "null_resource" "ecr_image" {
   # Runs the build.sh script which builds the dockerfile and pushes to ecr
   provisioner "local-exec" {
     command = "sudo bash build.sh ${aws_ecr_repository.repo.repository_url}:latest"
-    interpreter = ["bash", "-c"]
+    
   }
 }
 
