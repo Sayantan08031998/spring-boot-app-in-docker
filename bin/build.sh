a3886c9121ef7c624a46bd0488b4f1fe59148e15
Debatri2021 <92325478+Debatri2021@users.noreply.github.com>
2021-11-09 16:02:40 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,14 +4,13 @@
 set -e
 
 # This is the order of arguments
-build_folder=$1
-aws_ecr_repository_url_with_tag=$2
+aws_ecr_repository_url_with_tag=$1
 
 # Some Useful Debug
 echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
 
 # Build image
-docker build -t $aws_ecr_repository_url_with_tag $build_folder
+docker build -t $aws_ecr_repository_url_with_tag .
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
