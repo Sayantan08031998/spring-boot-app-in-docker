56710d0e234480b589d38231461920edceb7151e
Debatri2021 <92325478+Debatri2021@users.noreply.github.com>
2021-11-09 20:22:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ aws_ecr_repository_url_with_tag=$1
 # Some Useful Debug
 echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
 
-eval $( aws ecr get-login --no-include-email --region ap-south-1 )
-
 # Build image
 docker build -t $aws_ecr_repository_url_with_tag .
+
+aws ecr get-login --no-include-email
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
