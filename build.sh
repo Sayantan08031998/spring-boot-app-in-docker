e5eeee3220817d2e56f2e39c275451841ff5ab07
kshkraja <50064073+kshkraja@users.noreply.github.com>
2021-11-10 13:31:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@
 set -e
 
 # This is the order of arguments
-aws_ecr_repository_url_with_tag=$1
-
-aws_ecr_repository_url_with_tag=$1
+aws_ecr_repository_url=$1
+aws_ecr_repository_url_with_tag=$2
 
 # Some Useful Debug
 echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
@@ -18,7 +17,7 @@ aws ecr get-login-password \
     --region ap-south-1 \
 | docker login \
     --username AWS \
-    --password-stdin 749266080072.dkr.ecr.ap-south-1.amazonaws.com
+    --password-stdin $aws_ecr_repository_url
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
