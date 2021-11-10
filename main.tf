c1901225f4d0aa625ff3eca6d6189e45bab62b0f
kshkraja <50064073+kshkraja@users.noreply.github.com>
2021-11-10 18:51:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,11 +58,23 @@ EOF
 #  value = data.aws_ecr_repository.service.repository_url
 # }
 
+
 # added after radha's discussion
+
+data "archive_file" "server_bundle" {
+  type        = "zip"
+  source_dir  = "${path.root}"
+  output_path = "${path.root}"
+}
+
+
 resource "null_resource" "ecr_image" {
   triggers = {
-    #hash = data.external.hash.result["hash"]
     #timestamp        = timestamp()
+    #source_dir       = "${path.root}"
+    #output_path      = "${path.module}/${local.serverBundleFileName}"
+    #server_env_file  = local_file.server_env.filename
+    server_bundle_hash = data.archive_file.server_bundle.output_base64sha256
   }
   # Runs the build.sh script which builds the dockerfile and pushes to ecr
   provisioner "local-exec" {
@@ -71,4 +83,5 @@ resource "null_resource" "ecr_image" {
   }
 }
 
-//data.aws_caller_identity.current.account_id
+
+#data.aws_caller_identity.current.account_id
