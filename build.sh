095054ad30e10344672055bcfc27085e9c58f0cd
kshkraja <50064073+kshkraja@users.noreply.github.com>
2021-11-10 13:27:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -e
 # This is the order of arguments
 aws_ecr_repository_url_with_tag=$1
 
+aws_ecr_repository_url_with_tag=$1
+
 # Some Useful Debug
 echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
 
